8cd8f315bbaf23dc10b03e21b00d9c3ecf173e2d
TobCar <TobCar@users.noreply.github.com>
2019-02-18 17:23:59 -0500

    Set up Xcode project

---
+++ WaldoCV/main.cpp
@@ -0,0 +1,11 @@
+//
+//  Created by Tobias Carryer on 2019-02-18.
+//
+
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    std::cout << "Hello, World!\n";
+    return 0;
+}
