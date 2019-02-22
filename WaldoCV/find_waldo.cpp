84228a151c1d1fc8c22c409a2ab8ec83b04b930b
TobCar <TobCar@users.noreply.github.com>
2019-02-22 14:32:38 -0500

    Find Waldo using template matching

--- WaldoCV/find_waldo.cpp
+++ WaldoCV/find_waldo.cpp
@@ -1,4 +1,6 @@
-#include <opencv2/opencv.hpp>
+#include <opencv2/core/mat.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 #include <iostream>
 #include "find_waldo.h"
 
@@ -9,6 +11,7 @@ struct ImageNotFoundException : public std::exception {
 };
 
 int main(int argc, char** argv) {
+    // Load in the puzzle image and the picture of Waldo we are looking for
     cv::Mat puzzleImage;
     cv::Mat waldoTemplateImage;
     try {
@@ -19,9 +22,13 @@ int main(int argc, char** argv) {
         return -1;
     }
     
-    // Display the image to test we loaded it properly
-    namedWindow("Puzzle Image", cv::WINDOW_AUTOSIZE);
-    cv::imshow("Puzzle Image", puzzleImage);
+    // Find Waldo and show the user where he is
+    cv::Point matchLoc = findWaldoWithTemplateMatching(puzzleImage, waldoTemplateImage);
+    
+    cv::rectangle(puzzleImage, matchLoc, cv::Point(matchLoc.x + waldoTemplateImage.cols , matchLoc.y + waldoTemplateImage.rows), cv::Scalar::all(0), 2, 8, 0);
+    
+    cv::imshow("Window", puzzleImage);
+    cv::imshow("Window", puzzleImage );
     cv::waitKey(0);
     
     return 0;
@@ -35,4 +42,24 @@ cv::Mat loadImage(std::string imageName) {
     }
     
     return image;
+}
+
+cv::Point findWaldoWithTemplateMatching(cv::Mat img, cv::Mat templ) {
+    // Create the result matrix
+    int result_cols =  img.cols - templ.cols + 1;
+    int result_rows = img.rows - templ.rows + 1;
+
+    cv::Mat result;
+    result.create( result_rows, result_cols, CV_32FC1 );
+    
+    // Calculate the matches at each possible position
+    cv::matchTemplate(img, templ, result, cv::TM_CCOEFF);
+    cv::normalize(result, result, 0, 1, cv::NORM_MINMAX, -1, cv::Mat());
+    
+    // Locate the best match
+    double minVal; double maxVal; cv::Point minLoc; cv::Point maxLoc;
+    cv::minMaxLoc(result, &minVal, &maxVal, &minLoc, &maxLoc, cv::Mat());
+    cv::Point matchLoc = maxLoc;
+    
+    return matchLoc;
 }
--- WaldoCV/find_waldo.h
+++ WaldoCV/find_waldo.h
@@ -1,8 +1,10 @@
 #ifndef find_waldo_h
 #define find_waldo_h
 
-#include <opencv2/opencv.hpp>
+#include <opencv2/core/mat.hpp>
+#include <opencv2/core/types.hpp>
 
 cv::Mat loadImage(std::string);
+cv::Point findWaldoWithTemplateMatching(cv::Mat img, cv::Mat templ);
 
 #endif
